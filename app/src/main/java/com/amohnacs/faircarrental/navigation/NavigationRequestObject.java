098acda75e78ffdd1f0bd896c525e71919958d04
Adrian Mohnacs <amohnacss@gmail.com>
2018-04-24 23:54:49 -0700

    Updating Gradle indices

---
+++ app/src/main/java/com/amohnacs/faircarrental/navigation/NavigationRequestObject.java
@@ -0,0 +1,40 @@
+package com.amohnacs.faircarrental.navigation;
+
+import com.google.maps.model.TravelMode;
+
+public class NavigationRequestObject {
+
+    private TravelMode mode;
+    private String originLatLng;
+    private String destinationLatLng;
+
+    public NavigationRequestObject(TravelMode mode, String originLatLng, String destinationLatLng) {
+        this.mode = mode;
+        this.originLatLng = originLatLng;
+        this.destinationLatLng = destinationLatLng;
+    }
+
+    public TravelMode getMode() {
+        return mode;
+    }
+
+    public void setMode(TravelMode mode) {
+        this.mode = mode;
+    }
+
+    public String getOriginLatLng() {
+        return originLatLng;
+    }
+
+    public void setOriginLatLng(String originLatLng) {
+        this.originLatLng = originLatLng;
+    }
+
+    public String getDestinationLatLng() {
+        return destinationLatLng;
+    }
+
+    public void setDestinationLatLng(String destinationLatLng) {
+        this.destinationLatLng = destinationLatLng;
+    }
+}
